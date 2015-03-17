5cc2bab2ef80596703e8393bba35f6adb4adb5b7
Matt McCoy <mattnenterprise@yahoo.com>
2015-03-17 14:12:38 -0400

    Sending signals from gogo to the currently running job

--- main.go
+++ main.go
@@ -7,33 +7,61 @@ import (
 	"github.com/gorhill/cronexpr"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
+	signals := createSignalChannel()
 	jobs, err := initializeJobs()
 	if err != nil {
 		fmt.Println("initializeJobs: " + err.Error())
 	}
 	fmt.Println(jobs)
 	for {
-		timeUntilNextJob, _ := prepExecution(jobs)
+		timeUntilNextJob, _ := prepExecution(jobs, signals)
+		done := make(chan bool, 1)
+		go func() {
+			for {
+				select {
+				case <-signals:
+					os.Exit(0)
+				case <-done:
+					return
+				}
+			}
+		}()
 		time.Sleep(timeUntilNextJob)
+		done <- true
 	}
 }
 
-func waitForProcessCompletion(command string, args []string) {
+func createSignalChannel() chan os.Signal {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	return c
+}
+
+func waitForProcessCompletion(signals chan os.Signal, command string, args []string) {
 	execute := exec.Command(command, args...)
 	done := make(chan error, 1)
 	execute.Stdout = os.Stdout
 	execute.Stderr = os.Stderr
 	execute.Start()
+	proc := execute.Process
+	var exit bool
+	exit = false
 	go func() {
 		done <- execute.Wait()
 	}()
 	for {
 		select {
 		case err := <-done:
+			if exit {
+				os.Exit(0)
+			}
 			if err != nil {
 				fmt.Println("Process finished with error: " + err.Error())
 				return
@@ -41,17 +69,20 @@ func waitForProcessCompletion(command string, args []string) {
 				fmt.Println("Process finished successfully.")
 				return
 			}
+		case signal := <-signals:
+			proc.Signal(signal)
+			exit = true
 		}
 	}
 }
 
-func prepExecution(jbs Jobs) (time.Duration, error) {
+func prepExecution(jbs Jobs, signals chan os.Signal) (time.Duration, error) {
 	jobs := jbs.Sorted()
 	if time.Now().Before(jobs[0].NextTime) {
 		return jobs[0].NextTime.Sub(time.Now()), nil
 	}
 	fmt.Println("Running job " + jobs[0].Name)
-	waitForProcessCompletion(jobs[0].Command, jobs[0].Args)
+	waitForProcessCompletion(signals, jobs[0].Command, jobs[0].Args)
 	jobs[0].NextTime = jobs[0].Expression.Next(time.Now())
 	return jobs.Sorted()[0].NextTime.Sub(time.Now()), nil
 }
